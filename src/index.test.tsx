2c9df20a7d22984b8b0d72b00d5ecdaf092542bc
Philip Andersson <philipodev@gmail.com>
2023-04-01 00:01:36 +0200

    initial commit

---
+++ src/index.test.tsx
@@ -0,0 +1,66 @@
+import { PropsWithChildren } from "react"
+import { assert, expect, test } from 'vitest'
+import reactElementToJson from '.'
+
+test('simple tree', () => {
+  const jsx = (
+    <Node prop1="testProp1">
+      <Node prop2={1}>
+        <Node prop3 />
+      </Node>
+    </Node>
+  )
+
+  const output = reactElementToJson(jsx)
+
+  assert.isNotNull(output)
+
+  expect(output?.props['prop1']).toBe('testProp1')
+  expect(output?.children).toHaveLength(1)
+
+  const child = output?.children && output.children[0]
+  expect(child?.props['prop2']).toBe(1)
+
+  const grandChild = child?.children && child.children[0]
+  expect(grandChild?.props['prop3']).toBe(true)
+})
+
+test('one node', () => {
+  const jsx = <Node />
+
+  const output = reactElementToJson(jsx)
+
+  assert.isNotNull(output)
+  expect(output?.children).toHaveLength(0)
+})
+
+test('null', () => {
+  const jsx = null
+
+  const output = reactElementToJson(jsx)
+
+  expect(output).toBeNull()
+})
+
+test("string children", () => {
+  const jsx = <Node>test</Node>
+
+  const output = reactElementToJson(jsx)
+
+  assert.isNotNull(output)
+  expect(output?.children).toHaveLength(1)
+
+  const child = output?.children && output.children[0]
+  expect(child?.type).toBe('string')
+  expect(child?.value).toBe('test')
+});
+
+interface NodeProps {
+  prop1?: string
+  prop2?: number
+  prop3?: boolean
+}
+
+export function Node({ children }: PropsWithChildren<NodeProps>) {
+  return <>{children}</>
+}
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import React, { ComponentType, ReactElement } from 'react'
+
+export type ReactElementJSON = {
+  type: string
+  props: Record<string, any>
+  children: (ReactElementJSON | null)[] | null
+  value?: any
+}
+
+export default function reactElementToJson<T>(
+  element: ReactElement | null
+): ReactElementJSON | null {
+  if (!element) {
+    return null
+  }
+
+  const { type, props, key } = element
+
+  const children = React.Children.toArray(props.children).map((child) => {
+    if (React.isValidElement(child)) {
+      return reactElementToJson(child as ReactElement)
+    } else {
+      return {
+        type: typeof child,
+        props: {},
+        children: [],
+        value: child,
+      } as ReactElementJSON
+    }
+  })
+
+  return {
+    type:
+      typeof type === 'string'
+        ? type
+        : type.name || ((type as ComponentType).displayName as string),
+    props: { ...props, key, children: undefined },
+    children,
+  }
+}
